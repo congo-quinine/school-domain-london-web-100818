298db1784c6c04ecd0da5fc1e0203d97957eeab7
Rob Parker <into.the.parker@gmail.com>
2018-10-10 15:49:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+require "pry"
+
+class School 
+  attr_reader :school
+
+  def initialize(school)
+    @school = school
+  end
+
+  def roster
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] = []
+    @roster[grade] << student_name
+   
+  end 
+  
+end
