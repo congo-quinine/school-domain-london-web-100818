9dadd1df0bb09e3462ba7206a34ab9ceb5b979af
Rob Parker <into.the.parker@gmail.com>
2018-10-10 15:51:19 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,12 +6,11 @@ class School
 
   def initialize(school)
     @school = school
-     @roster = {}
   end
 
-  # def roster
-  #   @roster = {}
-  # end
+  def roster
+    @roster = {}
+  end
 
   # def add_student(student_name, grade)
   #   @roster[grade] = []
